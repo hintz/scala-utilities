3d68989c26644a87285aee59d52f332904f6d18d
Gerold <gerold.hintz@gmail.com>
2015-12-21 16:02:28 +0100

    added option to supply cache file location

--- src/main/scala/de/tudarmstadt/langtech/scala_utilities/index_file/CachedPrefixIndexedFile.scala
+++ src/main/scala/de/tudarmstadt/langtech/scala_utilities/index_file/CachedPrefixIndexedFile.scala
@@ -3,10 +3,10 @@ package de.tudarmstadt.langtech.scala_utilities.index_file
 import de.tudarmstadt.langtech.scala_utilities.cache.FileBackedCache
 
 /** Wraps a PrefixIndexedFile with an additional lookup cache */
-class CachedPrefixIndexedFile(val path: String, val prefixLength: Int = 5) {
+class CachedPrefixIndexedFile(val path: String, val prefixLength: Int = 5, cachefile: String = null) {
   
   val prefixFile = new PrefixIndexedFile(path, prefixLength)
-  val cache = FileBackedCache(prefixFile.search, path + ".cache")
+  val cache = FileBackedCache(prefixFile.search, if(cachefile eq null) path + ".cache" else cachefile)
   
   def search(s: String) = cache(s)
 }
